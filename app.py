33fce8a29b18be72c069a40a8ec61278ef56836a
Maria Kekola <maria.kekola@helsinki.fi>
2025-10-27 16:25:30 +0200

    Added new patch form.

--- app.py
+++ app.py
@@ -12,6 +12,21 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_patch")
+def new_patch():
+    return render_template("new_patch.html")
+
+@app.route("/create_patch", methods=["POST"])
+def create_patch():
+    title = request.form["title"]
+    description = request.form["description"]
+    technique = request.form["technique"]
+    user_id = session["user_id"]
+
+    sql = "INSERT INTO patches (title, description, technique, user_id) VALUES (?, ?, ?, ?)"
+    db.execute(sql, [title, description, technique, user_id])
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -41,10 +56,13 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-    password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
 
     if check_password_hash(password_hash, password):
+        session["user_id"] = user_id
         session["username"] = username
         return redirect("/")
     else:
@@ -52,6 +70,7 @@ def login():
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
 
--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE patches (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    technique TEXT,
+    user_id INTEGER REFERENCES users
+);
