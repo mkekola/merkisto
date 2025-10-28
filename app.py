b503abcd7def837b194a9317158812a2ecb77d02
Maria Kekola <maria.kekola@helsinki.fi>
2025-10-28 15:59:15 +0200

    Refactor registration/login.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import sqlite3
 from flask import Flask
 from flask import abort, redirect, render_template, request, session
-from werkzeug.security import check_password_hash, generate_password_hash
 import config
 import db
 import patches
@@ -127,15 +126,13 @@ def create():
     password2 = request.form["password2"]
     if password1 != password2:
         return "VIRHE: salasanat eivät ole samat"
-    password_hash = generate_password_hash(password1)
 
     try:
-        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-        db.execute(sql, [username, password_hash])
+        users.create_user(username, password1)
     except sqlite3.IntegrityError:
-        return "VIRHE: tunnus on jo varattu"
+        return "VIRHE: käyttäjätunnus on jo olemassa" + redirect("/register")
 
-    return "Tunnus luotu"
+    return redirect("/login")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -145,17 +142,13 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        sql = "SELECT id, password_hash FROM users WHERE username = ?"
-        result = db.query(sql, [username])[0]
-        user_id = result["id"]
-        password_hash = result["password_hash"]
-
-    if check_password_hash(password_hash, password):
-        session["user_id"] = user_id
-        session["username"] = username
-        return redirect("/")
+        user_id = users.check_login(username, password)
+        if user_id:
+            session["user_id"] = user_id
+            session["username"] = username
+            return redirect("/")
     else:
-        return "VIRHE: väärä tunnus tai salasana"
+        return "VIRHE: väärä tunnus tai salasana" + redirect("/login")
 
 @app.route("/logout")
 def logout():
